9bd751d4f30a47dacbae2616e0d482803e3f8cbc
intus su <ice.intouch.ice@gmail.com>
2024-08-29 22:26:28 +0700

    adding about dat

---
+++ components/Home/About.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex flex-col items-center justify-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 h-svh align-middle">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900 m-4">{{ about.about }}</h1>
+        <div class="flex flex-row items-center justify-center">
+            <div class="basis-1/3 m-4">
+            <div class="flex flex-col">
+                <p v-for="(detail, index) in about.details" class="text m-4">
+                    {{ detail.label }}: {{ detail.value }}
+                </p>  
+                
+                    <a :href="about.Github" class="text-black m-4" target="_blank" rel="noopener noreferrer">GITHUB: {{ about.Github }}</a>
+                
+            </div>
+            </div>
+            <div class="basis-1/2 m-4">
+                <p class="text text-justify indent-8">{{ about.description }}</p> 
+            </div>     
+        </div>
+    </div>
+</template>
+
+<script setup>
+const about = useAbout().value
+</script>
---
+++ composables/About.js
@@ -0,0 +1,16 @@
+export const useAbout = () => {
+    return useState('about', () => ({
+        about: `ABOUT ME`,
+        details: [
+            { label: 'Full Name', value: 'Intouch Suwanthanee' },
+            { label: 'Email', value: 'intouch.sw@gmail.com' },
+            { label: 'Phone', value: '099-414-5292' },
+            { label: 'Address', value: 'Samsen Nai, Phaya Thai, Bangkok 10400' },
+        ],
+        Github: 'https://github.com/intouch1999',
+        description: `Currently an IT Support Officer with a strong passion for Web Development and
+ practical experience from freelance projects. Enthusiastic about transitioning to Web
+ Application Development, I bring technical skills, problem-solving abilities, and a
+ commitment to continuous growth to contribute effectively to your team.`,
+    }));
+}
