2f179aa03037f9af175d90a9c134a5c5d1351cc1
intouch <ice.intouch.ice@gmail.com>
2024-08-29 17:23:31 +0700

    set layout component profile

---
+++ components/Home/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center justify-center my-10 ">
+        <NuxtImg 
+        src="/img/personal.jpg"  
+        sizes="100vw sm:50vw md:33vw lg:25vw"
+        class="rounded-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+         />
+        <div class="flex flex-col">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900 opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-100 "> Name </h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900  "> Name </h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900"> about me </h1>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,16 @@
 <template>
-    <div>
-    </div>
-</template>
+<header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold tracking-tight text-gray-900 animate-fade-in">{{ Headers }}</h1>
+        </div>
+      </header>
+      <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <HomeProfile />
+        </div>
+      </main>
+  </template>
+  
+  <script setup>
+ 
+  </script>
