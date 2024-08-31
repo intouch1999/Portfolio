8937b37caf34cbd6aaca2cfba899324dafb6bece
intus su <ice.intouch.ice@gmail.com>
2024-08-31 22:01:59 +0700

    adding fade in

--- app.vue
+++ app.vue
@@ -5,20 +5,4 @@
 </template>
 
 <style>
-  .fade-in-element {
-    opacity: 0;
-    animation: fadeIn 0.5s ease-out forwards;
-    animation-delay: calc(var(--stagger) * 10ms);
-  }
-  
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-      transform: translateY(20px);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
 </style>
