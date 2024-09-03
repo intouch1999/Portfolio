ca9e1923375bf4dba25b1a920c6919a0a2337c73
intus su <ice.intouch.ice@gmail.com>
2024-09-03 22:34:11 +0700

    add main component for build

--- components/Home/About.vue
+++ components/Home/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center justify-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 h-svh align-middle m-8 ">
+    <div class="flex flex-col items-center justify-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 h-svh align-middle m-4 ">
       <div data-aos="fade-up" class="flex flex-col items-center justify-center text-center">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900 m-4">{{ about.about }}</h1>
         <div class="flex flex-col md:flex-row items-center justify-center w-full">
---
+++ components/Home/Skill.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="flex flex-col items-center justify-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 h-svh align-middle m-4 ">
+            <div data-aos="fade-up" class="flex flex-col items-center justify-center text-center">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900 m-4">Skills</h1>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>JavaScript</p>
+                <p>Jquery</p>
+                <p>Bootstrap</p>
+                <p>Nuxt.js</p>
+                <p>Vue.js</p>
+                <p>PHP</p>
+                <p>Node.js</p>
+                <p>Express.js</p>
+                <p>MySQL</p>
+                <p>PostgreSQL</p>
+                <p>Git</p>
+                <p>Postman</p>
+    </div>
+    </div>
+    </div>
+</template>
