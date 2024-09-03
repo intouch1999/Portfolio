4905878fa21a0e8949bab2af4b6adba528da8379
intus su <ice.intouch.ice@gmail.com>
2024-09-03 23:01:54 +0700

    mock skills logo

--- components/Home/Skill.vue
+++ components/Home/Skill.vue
@@ -1,22 +1,65 @@
+
 <template>
     <div>
         <div class="flex flex-col items-center justify-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 h-svh align-middle m-4 ">
             <div data-aos="fade-up" class="flex flex-col items-center justify-center text-center">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900 m-4">Skills</h1>
-                <p>HTML</p>
-                <p>CSS</p>
-                <p>JavaScript</p>
-                <p>Jquery</p>
-                <p>Bootstrap</p>
-                <p>Nuxt.js</p>
-                <p>Vue.js</p>
-                <p>PHP</p>
-                <p>Node.js</p>
-                <p>Express.js</p>
-                <p>MySQL</p>
-                <p>PostgreSQL</p>
-                <p>Git</p>
-                <p>Postman</p>
+                <div>
+                    <NuxtImg src="/img/skills/html.svg" class="w-24 h-24" />
+                    <p>HTML</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/css.svg" class="w-24 h-24" />
+                    <p>CSS</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/javascript.svg" class="w-24 h-24" />
+                    <p>JavaScript</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/jquery.svg" class="w-24 h-24" />
+                    <p>Jquery</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/bootstrap.svg" class="w-24 h-24" />
+                    <p>Bootstrap</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/nuxt.svg" class="w-24 h-24" />
+                    <p>Nuxt.js</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/vue.svg" class="w-24 h-24" />
+                    <p>Vue.js</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/PHP.svg" class="w-24 h-24" />
+                    <p>PHP</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/nodejs.svg" class="w-24 h-24" />
+                    <p>Node.js</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/express.svg" class="w-24 h-24" />
+                    <p>Express.js</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/mysql.svg" class="w-24 h-24" />
+                    <p>MySQL</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/postgresql.svg" class="w-24 h-24" />
+                    <p>PostgreSQL</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/git.svg" class="w-24 h-24" />
+                    <p>Git</p>
+                </div>
+                <div>
+                    <NuxtImg src="/img/skills/postman.svg" class="w-24 h-24" />
+                    <p>Postman</p>
+                </div>
     </div>
     </div>
     </div>
