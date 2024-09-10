2e89a2717cf0fae048897653054980cccb7f8db4
intus su <ice.intouch.ice@gmail.com>
2024-09-10 22:07:31 +0700

    projects progress:40%

--- composables/projects.js
+++ composables/projects.js
@@ -4,12 +4,27 @@ export const useProjects = () => {
             {
                 id: 1,
                 name: 'Pawn Shop Management System',
-                img: '/img/Pawn_Sh/pawn-01.png',
+                lang: [
+                    'PHP', 'JavaScript', 'MySQL', 'HTML', 'CSS' 
+                ],
+                img: [
+                    '/img/Pawn_Sh/pawn-01.png',
+                    '/img/Pawn_Sh/pawn-02.png',
+                    '/img/Pawn_Sh/pawn-03.png',
+                    '/img/Pawn_Sh/pawn-04.png',
+                    '/img/Pawn_Sh/pawn-05.png',
+                ]
             },
             {
                 id: 2,
                 name: 'Water Delivery Management System',
-                img: '/img/Water_Deli/water-01.png',
+                img: [
+                    '/img/Water_Deli/water-01.png',
+                    '/img/Water_Deli/water-02.png',
+                    '/img/Water_Deli/water-03.png',
+                    '/img/Water_Deli/water-04.png',
+                    '/img/Water_Deli/water-05.png',
+                ]
             },
         ]
     }))
