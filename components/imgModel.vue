f6360a3f02eb31879646ead857c6a94cf3777c6e
intus su <ice.intouch.ice@gmail.com>
2024-09-10 20:56:00 +0700

    create img popup

---
+++ components/imgModel.vue
@@ -0,0 +1,81 @@
+<template>
+    <transition name="fade">
+      <div
+        v-if="isOpen"
+        @click.self="closeModal"
+        class="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+      >
+        <div class="relative">
+          <button
+            @click="closeModal"
+            class="absolute top-0 right-0 text-white text-3xl px-4 py-2"
+          >
+            &times;
+          </button>
+          <transition name="zoom">
+            <NuxtImg
+              v-if="isOpen"
+              class="rounded-xl max-w-full max-h-screen"
+              :src="imgSrc"
+              alt="Modal Image"
+              loading="lazy"
+              ismap=""
+              decoding="auto"
+            />
+          </transition>
+        </div>
+      </div>
+    </transition>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  });
+  
+  const emit = defineEmits(['close']);
+  
+  const closeModal = () => {
+    emit('close');
+  };
+  </script>
+  
+  <style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  
+  .zoom-enter-active,
+  .zoom-leave-active {
+    transition: transform 0.5s;
+  }
+  .zoom-enter,
+  .zoom-leave-to {
+    transform: scale(0.8);
+  }
+  
+  .modal-container {
+    max-width: 90%;
+    max-height: 90%;
+  }
+  
+  @media (min-width: 768px) {
+    .modal-container {
+      max-width: 70%;
+      max-height: 70%;
+    }
+  }
+  </style>
+  
