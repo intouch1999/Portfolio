fc9cd379ca9326bf772e4fa59a20935cab558ef8
intouch <ice.intouch.ice@gmail.com>
2024-09-13 17:20:26 +0700

    selected nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     <nav :class="['fixed w-full bg-white shadow z-50 opacity-90 transition-transform duration-300', { 'transform -translate-y-full': isNavHidden }]">
       <div class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center z-50">
         <div class="flex items-center justify-between">
-          <NuxtLink href="/" class="">
+          <NuxtLink exact href="/" class="">
             <h2 class="text-2xl font-bold text-gray-800 md:text-3xl"  > <span class="text-blue-600">P</span>ortfol<span class="text-blue-600">io</span></h2>
           </NuxtLink>
           <div class="flex lg:hidden">
@@ -21,8 +21,22 @@
         </div>
         <div :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-50 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
           <div class="flex flex-col md:flex-row md:mx-6">
-            <NuxtLink class="my-2 text-gray-700 transition-colors duration-300 transform hover:text-blue-500 md:mx-4 md:my-0" to="/">Home</NuxtLink>
-            <NuxtLink class="my-2 text-gray-700 transition-colors duration-300 transform hover:text-blue-500 md:mx-4 md:my-0" to="/projects">Projects</NuxtLink>
+<NuxtLink
+  exact
+  class="nav-link my-2 text-gray-700 transition-all duration-300 transform hover:border hover:bg-blue-600 hover:text-white hover:border-blue-500 rounded-lg hover:px-3 hover:py-1 md:px-3 md:py-1 md:mx-2 md:my-auto"
+  to="/"
+>
+  Home
+</NuxtLink>
+
+<NuxtLink
+  class="nav-link my-2 text-gray-700 transition-all duration-300 transform hover:border hover:bg-blue-600 hover:text-white hover:border-blue-500 rounded-lg hover:px-3 hover:py-1 md:px-3 md:py-1 md:mx-2 md:my-auto"
+  to="/projects"
+>
+  Projects
+</NuxtLink>
+
+
           </div>
         </div>
       </div>
@@ -53,6 +67,7 @@ const showButton = ref(false);
 const isOpen = ref(false);
 const lastScrollTop = ref(0);
 const isNavHidden = ref(false);
+
 
 const scrollToTop = () => {
   window.scrollTo({
@@ -90,3 +105,10 @@ const closeMenu = () => {
   isOpen.value = false;
 };
 </script>
+
+<style>
+.nav-link.router-link-active {
+  @apply border bg-blue-600 text-white border-blue-500 rounded-lg px-3 py-1;
+}
+
+</style>
