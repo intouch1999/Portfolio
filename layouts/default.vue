b0e03886d9ecf756f2e4ea231e3cf63347304de3
intus su <ice.intouch.ice@gmail.com>
2024-09-15 22:42:28 +0700

    test deply

--- layouts/default.vue
+++ layouts/default.vue
@@ -51,7 +51,7 @@
   
   <footer>
     <div>
-      <button v-if="showButton" @click="scrollToTop" class="fixed bottom-8 right-8 bg-blue-600 text-white p-3 rounded-full shadow-lg hover:bg-blue-500 transition duration-300 z-10">
+      <button v-if="showButton" @click="scrollToTop" class="fixed bottom-8 right-8 bg-blue-600 text-white p-3 rounded-full shadow-lg hover:bg-blue-500 transition duration-300 z-50">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12.75l-7.5-7.5-7.5 7.5m15 6l-7.5-7.5-7.5 7.5" />
         </svg>
