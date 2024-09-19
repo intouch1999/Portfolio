123823c89ba04feced8d0192d2a6c00c83b281bd
intus su <ice.intouch.ice@gmail.com>
2024-09-19 22:37:02 +0700

    add img

--- components/Home/About.vue
+++ components/Home/About.vue
@@ -5,8 +5,8 @@
         <main class="relative z-20 w-full mt-8 md:flex md:items-center xl:mt-12">
             <div class="absolute w-full bg-white border-2 shadow-lg -z-10 md:h-96 rounded-2xl"></div>
             
-            <div class="w-full p-6 bg-white border-2 shadow-lg md:border-0 md:shadow-none md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
-                <img class="h-24 w-24 md:mx-6 rounded-full object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[26rem] md:rounded-2xl" src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="client photo" />
+            <div class=" w-full p-6 bg-white border-2 shadow-lg md:border-0 md:shadow-none md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
+                <NuxtImg class="mx-auto h-36 w-36 md:mx-6 rounded-full object-cover object-top shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[26rem] md:rounded-2xl" src="/img/intouch.jpg" alt="client photo" />
                 
                 <div class="mt-2 md:mx-6">
                     <div>
